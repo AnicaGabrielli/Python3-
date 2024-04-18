54fa7d166cd145dcdc464fcbc3e429411570be29
AnicaGabrielli <113793660+AnicaGabrielli@users.noreply.github.com>
2024-04-18 10:33:24 -0300

    Atualizar o 2.desafios.py

--- 2.desafios.py
+++ 2.desafios.py
@@ -1,9 +1,16 @@
 algo = input('digite algo: ')
-print('oyipo primitivo desse dado é',type(algo))
+print('o tipo primitivo desse dado é',type(algo))
+
 print('Esse dado é numérico:',algo.isnumeric())
+
 print('Esse dado é alfanumérico?',algo.isalnum())
+
 print('Esse dado é alpha:' ,algo.isalpha())
+
 print('Esse dado só contém espaços:', algo.isspace())
+
 print('Esse dado só contém maiúsculas:',algo.isupper())
+
 print('Esse dado só contém minusculas:',algo.islower())
+
 print("Esse dado está capitalizado:", algo.istitle())
