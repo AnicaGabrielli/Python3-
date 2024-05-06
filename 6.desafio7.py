7f22c8c8745b90d396e70221403e61d05c5e415e
AnicaGabrielli <113793660+AnicaGabrielli@users.noreply.github.com>
2024-05-06 18:49:20 -0300

    Update and rename 6. desafio 7.py to 6.desafio7.py

---
+++ 6.desafio7.py
@@ -0,0 +1,8 @@
+''escreva um progama que pergunte o salário de um funcionário e de o valor do seu aumento. 
+Para salários superiores a R$ 1250, calcule um aumento de 10%, para inferiores ou iguais, o aumento é de 15%.'''
+salario = float(input('Digite o seu salario? '))
+if salario > 1250:
+    salario = salario * 1.10
+else:
+    salario = salario*1.15
+print(f'Seu salário atual com aumento é {salario}')
